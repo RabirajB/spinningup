90e569b5fc49bd3dd901df9e685bf36ac8998ca6
RabirajB <rabiraj2010@gmail.com>
2024-09-10 17:48:05 +0200

    Updated simple_pg.py with gymansium
    Fized the errors regarding observation and incorporated the new open AI gymnasium for teaching the environments

--- spinup/examples/pytorch/pg_math/1_simple_pg.py
+++ spinup/examples/pytorch/pg_math/1_simple_pg.py
@@ -3,9 +3,13 @@ import torch.nn as nn
 from torch.distributions.categorical import Categorical
 from torch.optim import Adam
 import numpy as np
-import gym
-from gym.spaces import Discrete, Box
-
+import gymnasium as gym
+from gymnasium.spaces import Discrete, Box
+import copy
+'''
+Below is defined the network which is essentially the policy for the agent, it is a neural network which takes into 
+account the observation dimensions.
+'''
 def mlp(sizes, activation=nn.Tanh, output_activation=nn.Identity):
     # Build a feedforward neural network.
     layers = []
@@ -14,25 +18,29 @@ def mlp(sizes, activation=nn.Tanh, output_activation=nn.Identity):
         layers += [nn.Linear(sizes[j], sizes[j+1]), act()]
     return nn.Sequential(*layers)
 
-def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2, 
+# The env-name is crucial and tells the code the pre-defined environment that will be used for testing the algorithm
+def train(env_name='CartPole-v1', hidden_sizes=[32], lr=1e-2, 
           epochs=50, batch_size=5000, render=False):
 
     # make environment, check spaces, get obs / act dims
     env = gym.make(env_name)
+    # This right here is demarcating the continuous space represented by the box and discrete ones represented by Discrete
     assert isinstance(env.observation_space, Box), \
         "This example only works for envs with continuous state spaces."
     assert isinstance(env.action_space, Discrete), \
         "This example only works for envs with discrete action spaces."
-
+    # the observation space dimension is the input and the action space is the output. For language this can be a discrete vocabulary
+    # this leads to the intuition of designing rewards based on tokens rather than the whole sentences which by itself is also an action space.
+    #n_acts defines the action space of the environment left or right
     obs_dim = env.observation_space.shape[0]
     n_acts = env.action_space.n
-
     # make core of policy network
     logits_net = mlp(sizes=[obs_dim]+hidden_sizes+[n_acts])
 
     # make function to compute action distribution
     def get_policy(obs):
         logits = logits_net(obs)
+        #This is a function which returns the action by sampling from the logits by modeling it as a Categorical distribution
         return Categorical(logits=logits)
 
     # make action selection function (outputs int actions, sampled from policy)
@@ -44,7 +52,7 @@ def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2,
         logp = get_policy(obs).log_prob(act)
         return -(logp * weights).mean()
 
-    # make optimizer
+    # make optimizer, this will be used for training the policy
     optimizer = Adam(logits_net.parameters(), lr=lr)
 
     # for training policy
@@ -55,54 +63,48 @@ def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2,
         batch_weights = []      # for R(tau) weighting in policy gradient
         batch_rets = []         # for measuring episode returns
         batch_lens = []         # for measuring episode lengths
-
+        ep_rews = []                # list for rewards accrued throughout ep
         # reset episode-specific variables
-        obs = env.reset()       # first obs comes from starting distribution
-        done = False            # signal from environment that episode is over
-        ep_rews = []            # list for rewards accrued throughout ep
-
+        obs, info = env.reset()     # first obs comes from starting distribution
+        done = False                # signal from environment that episode is over
+        
         # render first episode of each epoch
         finished_rendering_this_epoch = False
 
-        # collect experience by acting in the environment with current policy
+        # collect experience by acting in the environment with current policy, this thing is called an episode
         while True:
-
             # rendering
             if (not finished_rendering_this_epoch) and render:
                 env.render()
 
             # save obs
-            batch_obs.append(obs.copy())
+            batch_obs.append(obs.copy()) # Copying the observations array of the tuple
 
             # act in the environment
             act = get_action(torch.as_tensor(obs, dtype=torch.float32))
-            obs, rew, done, _ = env.step(act)
-
+            #Every step of the action gives the rewards from the environment
+            obs, reward, done, _, _ = env.step(act)
             # save action, reward
             batch_acts.append(act)
-            ep_rews.append(rew)
-
+            ep_rews.append(reward)
             if done:
                 # if episode is over, record info about episode
                 ep_ret, ep_len = sum(ep_rews), len(ep_rews)
                 batch_rets.append(ep_ret)
                 batch_lens.append(ep_len)
-
+                print("Episode Length", ep_len)
                 # the weight for each logprob(a|s) is R(tau)
-                batch_weights += [ep_ret] * ep_len
+                batch_weights+= [ep_ret] * ep_len
 
                 # reset episode-specific variables
-                obs, done, ep_rews = env.reset(), False, []
-
+                obs, info = env.reset()
+                done, ep_rews = False, []
                 # won't render again this epoch
                 finished_rendering_this_epoch = True
-
                 # end experience loop if we have enough of it
                 if len(batch_obs) > batch_size:
                     break
-
         # take a single policy gradient update step
-        optimizer.zero_grad()
         batch_loss = compute_loss(obs=torch.as_tensor(batch_obs, dtype=torch.float32),
                                   act=torch.as_tensor(batch_acts, dtype=torch.int32),
                                   weights=torch.as_tensor(batch_weights, dtype=torch.float32)
@@ -120,7 +122,7 @@ def train(env_name='CartPole-v0', hidden_sizes=[32], lr=1e-2,
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env_name', '--env', type=str, default='CartPole-v0')
+    parser.add_argument('--env_name', '--env', type=str, default='CartPole-v1')
     parser.add_argument('--render', action='store_true')
     parser.add_argument('--lr', type=float, default=1e-2)
     args = parser.parse_args()
